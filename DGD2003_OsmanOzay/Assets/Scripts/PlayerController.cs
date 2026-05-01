15182f41b3f339814b270a13b99445c78d2bd786
osmannozayy <socialosmanozay@gmail.com>
2026-05-01 17:41:44 +0300

    Papers Collecting Added

--- DGD2003_OsmanOzay/Assets/Scripts/PlayerController.cs
+++ DGD2003_OsmanOzay/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public float jumpHeight = 1.5f;
     public float gravity = -19.62f;
     public float turnSmoothTime = 0.15f;
+    public float interactionDistance = 5f;
+    public LayerMask paperLayer;
 
     private float turnSmoothVelocity;
     private float velocityY;
@@ -66,6 +68,11 @@ public class PlayerController : MonoBehaviour
             animator.SetTrigger("Jump");
         }
 
+        if (Keyboard.current.eKey.wasPressedThisFrame)
+        {
+            Interact();
+        }
+
         velocityY += gravity * Time.deltaTime;
         finalMovement.y = velocityY;
 
@@ -77,5 +84,24 @@ public class PlayerController : MonoBehaviour
             targetMoveAmount = isSprinting ? 1f : 0.5f;
         }
         animator.SetFloat("MoveAmount", targetMoveAmount, 0.1f, Time.deltaTime);
+    }
+
+    void Interact()
+    {
+        Debug.DrawRay(playerCamera.position, playerCamera.forward * interactionDistance, Color.red, 2f);
+
+        if (Physics.Raycast(playerCamera.position, playerCamera.forward, out RaycastHit hit, interactionDistance))
+        {
+            Debug.Log("Hit: " + hit.collider.name + " | Layer: " + LayerMask.LayerToName(hit.collider.gameObject.layer));
+
+            if ((paperLayer.value & (1 << hit.collider.gameObject.layer)) > 0)
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+        else
+        {
+            Debug.Log("No hit");
+        }
     }
 }
