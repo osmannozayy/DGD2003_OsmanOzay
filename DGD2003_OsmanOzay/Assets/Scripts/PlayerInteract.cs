a98823dc75d9c07587fe700747d8a622cb13b0f4
osmannozayy <socialosmanozay@gmail.com>
2026-05-26 14:15:04 +0300

    Win and Lose panel, Try again, Play again, Quit buttons added, Timer added

--- DGD2003_OsmanOzay/Assets/Scripts/PlayerInteract.cs
+++ DGD2003_OsmanOzay/Assets/Scripts/PlayerInteract.cs
@@ -1,27 +1,58 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerInteract : MonoBehaviour
 {
-    public TextMeshProUGUI sayacYazisi;
+    public TextMeshProUGUI counterText;
+    public TextMeshProUGUI timerText;
     public float interactDistance = 5f;
+
+    [Header("Timer & UI Screens")]
+    public float timeLimit = 60f;
+    public GameObject winScreen;
+    public GameObject loseScreen;
 
     private int totalNotes = 0;
     private int collectedNotes = 0;
+    private bool isGameOver = false;
 
     void Start()
     {
         totalNotes = FindObjectsOfType<LectureNote>().Length;
 
-        if (sayacYazisi != null)
+        if (winScreen != null) winScreen.SetActive(false);
+        if (loseScreen != null) loseScreen.SetActive(false);
+
+        if (counterText != null)
         {
-            sayacYazisi.text = "Collected Notes: 0 / " + totalNotes;
+            counterText.text = "0 / " + totalNotes;
         }
     }
 
     void Update()
     {
+        if (isGameOver)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            return;
+        }
+
+        timeLimit -= Time.deltaTime;
+
+        if (timerText != null)
+        {
+            timerText.text = Mathf.Ceil(timeLimit).ToString();
+        }
+
+        if (timeLimit <= 0)
+        {
+            timeLimit = 0;
+            LoseGame();
+        }
+
         if (Keyboard.current != null && Keyboard.current.rKey.wasPressedThisFrame)
         {
             Ray ray = new Ray(transform.position, transform.forward);
@@ -34,15 +65,47 @@ public class PlayerInteract : MonoBehaviour
                     Destroy(hit.collider.gameObject);
                     collectedNotes++;
 
-                    if (sayacYazisi != null)
+                    if (counterText != null)
                     {
-                        if (collectedNotes >= totalNotes)
-                            sayacYazisi.text = "CONGRATULATIONS! You collected all the notes!";
-                        else
-                            sayacYazisi.text = "Collected Notes: " + collectedNotes + " / " + totalNotes;
+                        counterText.text = collectedNotes + " / " + totalNotes;
+                    }
+
+                    if (collectedNotes >= totalNotes)
+                    {
+                        WinGame();
                     }
                 }
             }
         }
+    }
+
+    void WinGame()
+    {
+        isGameOver = true;
+        if (winScreen != null) winScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void LoseGame()
+    {
+        isGameOver = true;
+        if (timerText != null) timerText.text = "0";
+        if (loseScreen != null) loseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void RetryGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
